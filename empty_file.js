a5cd30efe5526fbe88cc911a0f7fe94f46bfe766
Clemens Sauer <clemens.sauer@gmx.net>
2019-09-20 20:38:36 +0200

    files

---
+++ empty_file.js
@@ -0,0 +1,5 @@
+
+var age=29;
+var player_name=["Martin", "Thomas", "Peter", "Mathias", "Niki"];
+window. alert("Hi my name is Martin and I am "+age+text_old )
+window. alert("The most valuable player of the match is " + player_name[2] +".")
